29bf1ef9854253ed711a97b012b1816afc51dee5
Hibyehello <joshuadcox05@gmail.com>
2021-12-30 20:45:49 -0600

    Initial Commit

---
+++ QTui/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QTui
+//
+//  Created by Cox Family on 12/30/21.
+//  Copyright © 2021 Hibyehello. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
